394bda43aabe016011c41ea1327d452913b977b7
yousafsabir <myousafdev@gmail.com>
2025-03-02 13:09:10 +0500

    feat-ui: made button smaller for mobile devices

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex justify-center group items-center gap-2 px-8 py-[10px] rounded-[10px] border border-app-gray-3 whitespace-nowrap focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex justify-center group items-center gap-2 px-5 sm:px-8 py-2 sm:py-[10px] rounded-[10px] border border-app-gray-3 whitespace-nowrap focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
